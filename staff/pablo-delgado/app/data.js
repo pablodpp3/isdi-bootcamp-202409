fdd30dd0d16af11bbf71890587f05098a6dbf823
Blu <pablodelgado@Ps-MB.local>
2024-10-24 17:16:23 +0200

    add last version of app WIP #170

--- staff/pablo-delgado/app/data.js
+++
@@ -1,4 +0,0 @@
-var users = [
-    { name: 'Lola Flores', email: 'lola@flores.com', username: 'lolaflores', password: 'penapenitapena' },
-    { name: 'Dicaprio', email: 'di@caprio.com', username: 'dicaprio', password: 'nomasde25' },
-];
