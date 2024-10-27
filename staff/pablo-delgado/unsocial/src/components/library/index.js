6e851cd7be62a30c184c555e4220843619bd2b18
Blu <pablodelgado@Ps-MB.local>
2024-10-27 20:45:51 +0100

    add comment section WIP #170

---
+++ staff/pablo-delgado/unsocial/src/components/library/index.js
@@ -0,0 +1,15 @@
+import Button from './Button'
+import Field from './Field'
+import Form from './Field'
+import Input from './Field'
+import Label from './Label'
+import PasswordInput from './PasswordInput'
+
+export {
+    Button,
+    Field,
+    Form,
+    Input,
+    Label,
+    PasswordInput
+}
